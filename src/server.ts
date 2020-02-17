3335ba8eec3b5bfd85b9607d6e26cff8148bec0b
takemdarlington <takemdarlington@gmail.com>
2020-02-17 14:13:53 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+
+
+  app.get( "/filteredimage", async ( req , res ) => {
+
+    let image_url:string = req.query.image_url;
+
+    if( !image_url ){
+      return res.status(400).send("Invalid Request!, please include an image url")
+    }
+    let result:string = await filterImageFromURL(image_url);
+    res.status(200).sendFile(result, () => {deleteLocalFiles([result])});
+
+  })
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
